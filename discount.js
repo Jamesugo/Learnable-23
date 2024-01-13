6f1df0e49287b719103e24a105870870f22bd510
city <jamesugochukwu194@gmail.com>
2024-01-13 10:37:53 +0100

    commit new changes

--- discount.js
+++ discount.js
@@ -1,7 +1,1 @@
-"How old are you"
-INPUT person's age
-STORE person as age
-IF age < 15
-OUTPUT you are eligible for a discount
-ELSE
-OUTPUT you are not eligibe for a discount
+console.log ("hello word")
